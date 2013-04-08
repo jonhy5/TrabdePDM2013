a1424e9eb747e0c119a68fa4b12b21b0ff084722
unknown <Marco@Marco-PC.(none)>
2013-04-08 21:59:44 +0100

    Setup inicial do projecto ETAPA1

---
+++ TrabEtapa1/src/com/grupo5/trabetapa1/TimelineActivity.java
@@ -0,0 +1,22 @@
+package com.grupo5.trabetapa1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TimelineActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+
+}
