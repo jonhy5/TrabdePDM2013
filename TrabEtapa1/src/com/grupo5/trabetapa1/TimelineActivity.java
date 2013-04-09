cd7c10ac4c784320d37e6afe0cda4503b689ed17
joão <jcscosta8@hotmail.com>
2013-04-09 15:51:51 +0100

    Commit  para verificar o repositorio

--- TrabEtapa1/src/com/grupo5/trabetapa1/TimelineActivity.java
+++ TrabEtapa1/src/com/grupo5/trabetapa1/TimelineActivity.java
@@ -10,6 +10,7 @@ public class TimelineActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
+		//Alteração para verificar o repositorio
 	}
 
 	@Override
