d10de81e2d8aaa874e87760829eda5b454cb0966
joão <jcscosta8@hotmail.com>
2013-04-15 20:20:40 +0100

    ActionBar Menu

--- TrabEtapa1/src/com/grupo5/trabetapa1/PreferencesActivity.java
+++ TrabEtapa1/src/com/grupo5/trabetapa1/PreferencesActivity.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class TimelineActivity extends Activity {
+public class PreferencesActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_timeline);
+		setContentView(R.layout.activity_preferences);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+		getMenuInflater().inflate(R.menu.preferences, menu);
 		return true;
 	}
 
