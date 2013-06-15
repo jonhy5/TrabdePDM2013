78cbf55f6599370ee10ecde845620a04c9af0565
Morlis <Marco@Marco-PC.(none)>
2013-06-15 18:08:51 +0100

    Application FINISHED, finally.

--- TrabEtapa1/src/com/grupo5/trabetapa1/appwidget/TimelineWidgetProvider.java
+++ TrabEtapa1/src/com/grupo5/trabetapa1/appwidget/TimelineWidgetProvider.java
@@ -1,11 +1,19 @@
 package com.grupo5.trabetapa1.appwidget;
 
+import java.util.Date;
+
+import com.grupo5.trabetapa1.R;
+import com.grupo5.trabetapa1.activities.TimelineActivity;
 import com.grupo5.trabetapa1.contentprovider.StatusProvider;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.widget.RemoteViews;
 
 public class TimelineWidgetProvider extends AppWidgetProvider {
 	private final String[] projection = {
@@ -15,15 +23,25 @@ public class TimelineWidgetProvider extends AppWidgetProvider {
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 		
-		Cursor cursor = context.getContentResolver().query(StatusProvider.CONTENT_URI,
-                projection, null, null, null);
-		cursor.moveToFirst();
-		String msg = cursor.getString(0);
-        String user = cursor.getString(1);
-		long created = cursor.getLong(2);
+		Cursor cursor = context.getContentResolver().query(StatusProvider.CONTENT_URI, projection, null, null, StatusProvider.KEY_CREATEDAT + " desc LIMIT 1");
+		if(cursor.moveToFirst()) {
+			String msg = cursor.getString(0);
+	        String user = cursor.getString(1);
+			String created = new Date(cursor.getLong(2)).toString();
+
+			remoteView.setTextViewText(R.id.userTextView, user);
+			remoteView.setTextViewText(R.id.dateTextView, created);
+			remoteView.setTextViewText(R.id.messageTextView, msg);
+		}
+		cursor.close();
 		
+		Intent launchAppIntent = new Intent(context, TimelineActivity.class);
+		PendingIntent launchAppPendingIntent = PendingIntent.getActivity(context, 0, launchAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		remoteView.setOnClickPendingIntent(R.id.full_widget, launchAppPendingIntent);
 		
+		ComponentName widget = new ComponentName(context, TimelineWidgetProvider.class);
+		appWidgetManager.updateAppWidget(widget, remoteView);
 	}	
 }
